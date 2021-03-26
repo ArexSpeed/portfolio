cc7578dd5f71cbc88bc8da7e6b3da1919b75c3a3
Arek Cichocki <arexspeed95@gmail.com>
2021-03-26 11:14:32 +0100

    toggle menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import {useRef} from 'react'
 import {Link} from 'react-scroll'
+import { IoIosArrowBack } from 'react-icons/io';
+
 
 const Nav = () => {
+  const buttonRef = useRef(null)
+  const navRef = useRef(null)
+
+  const toggle = () => {
+    buttonRef.current.classList.toggle('active')
+    navRef.current.classList.toggle('hide')
+  }
+
   return (
-    <nav className="nav">
-      <button className="nav-button">{">"}</button>
+    <>
+    <div className="toggler">
+      <button className="toggler-button" ref={buttonRef} onClick={toggle}><IoIosArrowBack style={{fontSize: '25px', margin: 'auto'}} /></button>
+    </div>
+    <nav className="nav" ref={navRef}>
       <div className="nav-avatar" />
       <ul className="nav-menu">
-        <Link to="about" className="nav-link"><span className="nav-link-line"></span>ABOUT</Link>
+        <Link to="about" className="nav-link" ><div className="nav-link-line"></div>ABOUT</Link>
         <Link to="about" className="nav-link"><div className="nav-link-line"></div>PROJECTS</Link>
         <Link to="about" className="nav-link"><div className="nav-link-line"></div>SKILLS</Link>
         <Link to="about" className="nav-link"><div className="nav-link-line"></div>CONTACT</Link>
       </ul>
     </nav>
+    </>
   )
 }
 
