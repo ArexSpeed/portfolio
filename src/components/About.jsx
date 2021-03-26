c2c4561337ab16049eedfd3b24f91edcdc6801bb
Arek Cichocki <arexspeed95@gmail.com>
2021-03-26 20:41:07 +0100

    hero chg bg, flex

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="about" id="about">
+      {/* <div className="about-triangle-up"></div> */}
+      <section className="about-title">
+        <h1 className="about-title-text">
+          ABOUT ME
+        </h1> 
+      </section>
+      <section className="about-desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid similique doloremque harum possimus dolor enim cum facilis amet. Molestiae magnam ipsa, quisquam consectetur, voluptatibus quia sequi commodi facere inventore voluptate tempore? Rem laboriosam hic consectetur perferendis ipsum maiores debitis ab autem, iste assumenda eveniet itaque perspiciatis ut dolorum rerum quod cumque placeat omnis quibusdam est dolores ducimus dignissimos! Culpa quae magni soluta pariatur? Dicta ea ipsam adipisci minus iste, repellendus vel asperiores voluptatum incidunt non architecto error perferendis aliquam eveniet obcaecati vero molestiae necessitatibus dolorem, natus culpa doloremque unde tenetur corporis! Obcaecati ipsa dignissimos quo ab provident velit sint quod modi autem repellat rerum nesciunt nihil voluptatem optio voluptatibus laudantium dicta, possimus labore quam beatae sit aperiam exercitationem sequi? Aspernatur, sit! Unde obcaecati exercitationem praesentium dignissimos iusto ipsa excepturi cum, consequatur nemo dolores molestias quaerat? Atque esse possimus voluptate enim est qui aliquid saepe deserunt dolores! Facere expedita magni ab nulla atque tempora. Velit officiis deserunt pariatur magni accusantium corporis dolores nobis voluptatibus soluta expedita, eaque aspernatur maxime eos quibusdam perspiciatis assumenda numquam molestiae. Tempora atque debitis officia dolore, odio dolorem velit nisi facilis reiciendis saepe harum beatae alias magni aspernatur ea quas, vel at ad necessitatibus asperiores ipsum. Corporis.
+      </section>
+      <div className="about-triangle-down"></div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,30 +3,30 @@ import Tilt from "react-tilt";
 
 const Hero = () => {
   return (
-    <div>
+    <div className="hero">
       <section className="section-title">
         <h1 className="section-title-text">
           MY PORTFOLIO
         </h1> 
       </section>
       <section className="hero-banner">
+        <article className="hero-banner-title">
+          <Tilt className="hero-banner-name">
+              <h2 className="hero-banner-name-one name">AREK</h2>
+              <h3 className="hero-banner-name-two name">CICHOCKI</h3>
+            </Tilt>
+            <div className="hero-banner-logo">A9</div>
+            <Tilt className="hero-banner-name">
+              <h2 className="hero-banner-name-one software">SOFTWARE</h2>
+              <h3 className="hero-banner-name-two software">DEVELOPER</h3>
+            </Tilt>
+          </article>
+          <article className="hero-banner-img">
 
+          </article>
       </section>
       <section className="hero-desc">
-        <article className="hero-desc-title">
-        <Tilt className="hero-desc-name">
-            <h2 className="hero-desc-name-one name">AREK</h2>
-            <h3 className="hero-desc-name-two name">CICHOCKI</h3>
-          </Tilt>
-          <div className="hero-desc-logo">A9</div>
-          <Tilt className="hero-desc-name">
-            <h2 className="hero-desc-name-one software">SOFTWARE</h2>
-            <h3 className="hero-desc-name-two software">DEVELOPER</h3>
-          </Tilt>
-        </article>
-        <article className="hero-desc-info">
         Welcome ! I am glad that you are here on my porfolio website. Here you can know me a little get knowledge about my story and see all my projects. So let’s go, I hope you enjoy it ! 
-        </article>
       </section>
     </div>
   )
