0408584a7823a80a70b3cd5cf65a8fe1dfee934b
Arek Cichocki <arexspeed95@gmail.com>
2021-03-31 22:23:32 +0200

    projects first attempt

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
-import {SiHtml5,SiSass,SiReact, SiJavascript, SiFigma, SiInkscape, SiBootstrap, SiNextDotJs,
-  SiNodeDotJs, SiFirebase, SiGithub, SiWordpress, SiPhp, SiMongodb, SiHeroku} 
+import {SiHtml5,SiSass,SiReact,SiCss3,SiStyledComponents, SiJavascript, SiFigma, SiInkscape, SiBootstrap, SiNextDotJs,
+  SiNodeDotJs, SiFirebase, SiGithub, SiWordpress, SiPhp, SiMongodb, SiHeroku, SiYoutube, SiInstagram} 
 from 'react-icons/si'
 import {GiDatabase, GiOppression, GiFlexibleStar} from 'react-icons/gi'
 import {BiShow} from 'react-icons/bi'
@@ -77,6 +77,116 @@ const Project = ({title, desc, images, logo, stacks}) => {
                         <p className="projects-box-info-button-name">Node</p>
                       </div>
                     );
+                    case "HTML":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiHtml5
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">HTML</p>
+                      </div>
+                    );
+                    case "JavaScript":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiJavascript
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">JS</p>
+                      </div>
+                    );
+                    case "CSS":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiCss3
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">CSS</p>
+                      </div>
+                    );
+                    case "StyledComponent":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiStyledComponents
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">StyledComponents</p>
+                      </div>
+                    );
+                    case "Firebase":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiFirebase
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">Firebase</p>
+                      </div>
+                    );
+                    case "Mongo":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiMongodb
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">MongoDB</p>
+                      </div>
+                    );
+                    case "Heroku":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiHeroku
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">Heroku</p>
+                      </div>
+                    );
+                    case "PHP":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiPhp
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">PHP</p>
+                      </div>
+                    );
+                    case "Wordpress":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiWordpress
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">Wordpress</p>
+                      </div>
+                    );
+                    case "YouTube":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiYoutube
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">YouTube</p>
+                      </div>
+                    );
+                    case "Instagram":
+                    return (
+                      <div className="projects-box-info-button">
+                        <SiInstagram
+                          className="projects-box-info-button-icon"
+                          style={{ fontSize: "25px" }}
+                        />
+                        <p className="projects-box-info-button-name">Instagram</p>
+                      </div>
+                    );
                   default:
                     return (
                       <div className="projects-box-info-button">
@@ -89,11 +199,6 @@ const Project = ({title, desc, images, logo, stacks}) => {
                     );
                 }
               })}
-            
-            {/* <div className="projects-box-info-button">
-              <SiReact className="projects-box-info-button-icon" style={{fontSize: '25px'}} />
-              <p className="projects-box-info-button-name">React</p>
-            </div> */}
 
             <a href="#" target="_blank" referrerPolicy="no-referrer" className="projects-box-info-button">
               <BiShow className="projects-box-info-button-icon" style={{fontSize: '25px'}} />
--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -9,14 +9,75 @@ import xtech2 from '../images/xtech2.png'
 import xtech3 from '../images/xtech3.png'
 import xtech4 from '../images/xtech4.png'
 import xtech5 from '../images/xtech5.png'
+import axo1 from '../images/axo1.png'
+import axo2 from '../images/axo2.png'
+import axo3 from '../images/axo3.png'
+import axo4 from '../images/axo4.png'
+import axo5 from '../images/axo5.png'
+import clock1 from '../images/clock1.png'
+import clock2 from '../images/clock2.png'
+import clock3 from '../images/clock3.png'
+import clock4 from '../images/clock4.png'
+import clock5 from '../images/clock5.png'
+import cs1 from '../images/cs1.png'
+import cs2 from '../images/cs2.png'
+import cs3 from '../images/cs3.png'
+import cs4 from '../images/cs4.png'
+import cs5 from '../images/cs5.png'
+import gc1 from '../images/gc1.png'
+import gc2 from '../images/gc2.png'
+import gc3 from '../images/gc3.png'
+import gc4 from '../images/gc4.png'
+import gc5 from '../images/gc5.png'
+import mw1 from '../images/mw1.png'
+import mw2 from '../images/mw2.png'
+import mw3 from '../images/mw3.png'
+import mw4 from '../images/mw4.png'
+import mw5 from '../images/mw5.png'
+import neograd1 from '../images/neograd1.png'
+import neograd2 from '../images/neograd2.png'
+import neograd3 from '../images/neograd3.png'
+import neograd4 from '../images/neograd4.png'
+import neograd5 from '../images/neograd5.png'
+import rounder1 from '../images/rounder1.png'
+import rounder2 from '../images/rounder2.png'
+import rounder3 from '../images/rounder3.png'
+import rounder4 from '../images/rounder4.png'
+import rounder5 from '../images/rounder5.png'
+import sps1 from '../images/sps1.png'
+import sps2 from '../images/sps2.png'
+import sps3 from '../images/sps3.png'
+import sps4 from '../images/sps4.jpg'
+import sps5 from '../images/sps5.jpg'
+
+
+
 
 export const projects = [
+  {
+    title: 'AXOffice',
+    desc: 'Description for project',
+    images: [axo1, axo2, axo3, axo4,axo5],
+    logo: aleaguel,
+    stacks: ['React', 'Sass', 'Firebase'],
+    website: '',
+    github: ''
+  },
+  {
+    title: 'Neograd',
+    desc: 'Description for project',
+    images: [neograd1, neograd2, neograd3, neograd4,neograd5],
+    logo: aleaguel,
+    stacks: ['React', 'Sass'],
+    website: '',
+    github: ''
+  },
   {
     title: 'ALeague',
     desc: 'Description for project',
     images: [aleague1, aleague2, aleague3, aleague4,aleague5],
     logo: aleaguel,
-    stacks: ['React', 'Sass'],
+    stacks: ['React', 'Sass','Node', 'Mongo', 'Firebase', 'Heroku'],
     website: '',
     github: ''
   },
@@ -25,8 +86,63 @@ export const projects = [
     desc: 'Description for project',
     images: [xtech1, xtech2, xtech3, xtech4,xtech5],
     logo: aleaguel,
-    stacks: ['React', 'Node'],
+    stacks: ['React','StyledComponent' ,'Node', 'Heroku'],
     website: '',
     github: ''
-  }
+  },
+  {
+    title: 'Sport Story',
+    desc: 'Description for project',
+    images: [sps1, sps2, sps3, sps4,sps5],
+    logo: aleaguel,
+    stacks: ['Wordpress', 'YouTube', 'Instagram'],
+    website: '',
+    github: ''
+  },
+  {
+    title: 'MultiWords',
+    desc: 'Description for project',
+    images: [mw1, mw2, mw3, mw4,mw5],
+    logo: aleaguel,
+    stacks: ['React', 'Sass', 'Firebase', 'PHP'],
+    website: '',
+    github: ''
+  },
+  {
+    title: 'Galaxy Chat',
+    desc: 'Description for project',
+    images: [gc1, gc2, gc3, gc4,gc5],
+    logo: aleaguel,
+    stacks: ['React', 'CSS', 'Firebase'],
+    website: '',
+    github: ''
+  },
+  {
+    title: 'ClickScore',
+    desc: 'Description for project',
+    images: [cs1, cs2, cs3, cs4,cs5],
+    logo: aleaguel,
+    stacks: ['HTML', 'CSS', 'JavaScript'],
+    website: '',
+    github: ''
+  },
+  {
+    title: 'Clocks',
+    desc: 'Description for project',
+    images: [clock1, clock2, clock3, clock4,clock5],
+    logo: aleaguel,
+    stacks: ['HTML', 'CSS', 'JavaScript'],
+    website: '',
+    github: ''
+  },
+  {
+    title: 'Rounder',
+    desc: 'Description for project',
+    images: [rounder1, rounder2, rounder3, rounder4,rounder5],
+    logo: aleaguel,
+    stacks: ['React', 'CSS', 'Firebase'],
+    website: '',
+    github: ''
+  },
+  
 ]
