3e8949d952984b74ca1ce7237c7cf2f27eb9db25
Arek Cichocki <arexspeed95@gmail.com>
2021-03-31 19:20:26 +0200

    project carousel

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,16 @@
-import React, {useState} from 'react'
-import {AiFillStar} from 'react-icons/ai'
+import {useState} from 'react'
 import {SiSass} from 'react-icons/si'
 import {SiReact} from 'react-icons/si'
 import {BiShow} from 'react-icons/bi'
-import {GiReturnArrow} from 'react-icons/gi'
 import aleague1 from '../images/aleague1.png'
 import aleague2 from '../images/aleague2.png'
 import aleague3 from '../images/aleague3.png'
 import aleague4 from '../images/aleague4.png'
-
-
+import sps4 from '../images/sps4.jpg'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const Project = () => {
-  const [flipped, setFlipped] = useState(false)
   const logo = <SiSass className="projects-box-info-button-icon" style={{fontSize: '25px'}} />
 
   return (
@@ -21,25 +19,25 @@ const Project = () => {
           <span></span>
           <span></span>
           <span></span>
-          <div className="projects-box-logo">
+          <div className="projects-box-screens">
 
-          {/* <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
-          <div className="flip">
-          <img src={logo_axoffice} alt="" className="flip-logo" />
-          <button onClick={() => setFlipped(!flipped)} className="flip-button">
-            <GiReturnArrow className="flip-button-icon"/>
-          </button>
+          <Carousel showThumbs={false}>
+                <div>
+                    <img src={aleague1} alt="" style={{maxWidth: '400px'}}/>
+                </div>
+                <div>
+                    <img src={aleague2} alt="" style={{maxWidth: '400px'}}/>
+                </div>
+                <div>
+                    <img src={aleague4} alt="" style={{maxWidth: '100px'}}/>
+                </div>
+                <div>
+                    <img src={sps4} alt="" style={{maxWidth: '100px'}}/>
+                </div>
+            </Carousel>
+
           </div>
 
-          <div className="flip">
-          <img src={screen_axoffice} alt="" className="flip-logo"/>
-          <button onClick={() => setFlipped(!flipped)} className="flip-button">
-            <GiReturnArrow className="flip-button-icon"/>
-          </button>
-          </div>
-          </ReactCardFlip> */}
-              {/* <AiFillStar /> */}
-          </div>
           <div className="projects-box-info">
             <div className="projects-box-info-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt non quibusdam cum velit beatae assumenda vel odio, error quos porro. Nesciunt nulla aliquam quasi amet accusantium sapiente atque quaerat? Aliquid.
@@ -57,7 +55,6 @@ const Project = () => {
               <BiShow className="projects-box-info-button-icon" style={{fontSize: '25px'}} />
               <p className="projects-box-info-button-name">Site</p>
             </a>
-
               {/* <SiSass className="projects-box-info-icon" />
               
               <button className="projects-box-info-button">More</button>
@@ -67,5 +64,4 @@ const Project = () => {
         </article>
   )
 }
-
 export default Project
