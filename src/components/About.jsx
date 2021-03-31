7f93bab85e9cf3db4dce5db2dda0f5d85feb37a9
Arek Cichocki <arexspeed95@gmail.com>
2021-03-31 10:59:56 +0200

    expand about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,36 @@ import React from 'react'
 const About = () => {
   return (
     <div className="about" id="about">
-      {/* <div className="about-triangle-up"></div> */}
       <section className="section-title next">
         <h1 className="section-title-text">
           ABOUT ME
         </h1> 
       </section>
       <section className="about-desc">
-        <div className="about-desc-text">
+        
+        <h4 className="about-desc-title">Who Am I?</h4>
+        <div className="about-desc-circle"></div>
+        <p className="about-desc-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto possimus ex a doloremque officia cupiditate quidem. Eius, natus ab? Repellat quod tempora eos repellendus laborum aspernatur voluptas magnam voluptate expedita, nobis ducimus ratione nisi totam sint consequuntur! Reiciendis dignissimos sapiente voluptates ipsa, deserunt maxime obcaecati assumenda ut cumque. Repellendus?
+        </p>
+
+        <h4 className="about-desc-title">My story</h4>
+        <div className="about-desc-circle"></div>
+        <p className="about-desc-text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid similique doloremque harum possimus dolor enim cum facilis amet. Molestiae magnam ipsa, quisquam consectetur, voluptatibus quia sequi commodi facere inventore voluptate tempore? Rem laboriosam hic consectetur perferendis ipsum maiores debitis ab autem, iste assumenda eveniet itaque perspiciatis ut dolorum rerum quod cumque placeat omnis quibusdam est dolores ducimus dignissimos! Culpa quae magni soluta pariatur? Dicta ea ipsam adipisci minus iste, repellendus vel asperiores voluptatum incidunt non architecto error perferendis aliquam eveniet obcaecati vero molestiae necessitatibus dolorem, natus culpa doloremque unde tenetur corporis! Obcaecati ipsa dignissimos quo ab provident velit sint quod modi autem repellat rerum nesciunt nihil voluptatem optio voluptatibus laudantium dicta, possimus labore quam beatae sit aperiam exercitationem sequi? Aspernatur, sit! Unde obcaecati exercitationem praesentium dignissimos iusto ipsa excepturi cum, consequatur nemo dolores molestias quaerat? Atque esse possimus voluptate enim est qui aliquid saepe deserunt dolores! Facere expedita magni ab nulla atque tempora. Velit officiis deserunt pariatur magni accusantium corporis dolores nobis voluptatibus soluta expedita, eaque aspernatur maxime eos quibusdam perspiciatis assumenda numquam molestiae. Tempora atque debitis officia dolore, odio dolorem velit nisi facilis reiciendis saepe harum beatae alias magni aspernatur ea quas, vel at ad necessitatibus asperiores ipsum. Corporis.
-        </div>
+        </p>
+
+        <h4 className="about-desc-title">My hobbies</h4>
+        <div className="about-desc-circle"></div>
+        <p className="about-desc-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid similique doloremque harum possimus dolor enim cum facilis amet. Molestiae magnam ipsa, quisquam consectetur, voluptatibus quia sequi commodi facere inventore voluptate tempore? Rem laboriosam hic consectetur perferendis ipsum maiores debitis ab autem, iste assumenda eveniet itaque perspiciatis ut dolorum rerum quod cumque placeat omnis quibusdam est dolores ducimus dignissimos! Culpa quae magni soluta pariatur? Dicta ea ipsam adipisci minus iste, repellendus vel asperiores voluptatum incidunt non architecto error perferendis aliquam eveniet obcaecati vero molestiae necessitatibus dolorem, natus culpa doloremque unde tenetur corporis! Obcaecati ipsa dignissimos quo ab provident velit sint quod modi autem repellat rerum nesciunt nihil voluptatem optio voluptatibus laudantium dicta, possimus labore quam beatae sit aperiam exercitationem sequi? Aspernatur, sit! Unde obcaecati exercitationem praesentium dignissimos iusto ipsa excepturi cum, consequatur nemo dolores molestias quaerat? Atque esse possimus voluptate enim est qui aliquid saepe deserunt dolores! Facere expedita magni ab nulla atque tempora. Velit officiis deserunt pariatur magni accusantium corporis dolores nobis voluptatibus soluta expedita, eaque aspernatur maxime eos quibusdam perspiciatis assumenda numquam molestiae. Tempora atque debitis officia dolore, odio dolorem velit nisi facilis reiciendis saepe harum beatae alias magni aspernatur ea quas, vel at ad necessitatibus asperiores ipsum. Corporis.
+        </p>
+
+        <h4 className="about-desc-title">My plans</h4>
+        <div className="about-desc-circle"></div>
+        <p className="about-desc-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sequi commodi, ea blanditiis cumque eaque dolor, ex ad amet obcaecati quisquam quibusdam laborum, repellat vero ducimus omnis assumenda quasi rerum cupiditate reprehenderit debitis ipsum! Et, corrupti rem. Exercitationem, corrupti nulla, sint quam amet enim magni odit accusantium dolorum quod soluta?
+        </p>
       </section>
       {/* <svg>
       <filter id="wavy">
@@ -22,7 +42,6 @@ const About = () => {
         <feDisplacementMap in="SourceGraphic" scale="30" />
       </filter>
     </svg> */}
-      {/* <div className="about-triangle-down"></div> */}
     </div>
   )
 }
