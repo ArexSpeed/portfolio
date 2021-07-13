2af12077b544f7e0205b51fbc31cefe7078123fc
Arek Cichocki <arexspeed95@gmail.com>
2021-07-13 17:01:08 +0200

    hero recreate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Tilt from "react-tilt";
 import {AiFillGithub} from 'react-icons/ai'
-import logo from '../images/herologo.png'
+import {Link} from 'react-scroll'
 import profile from '../images/heropic.png'
 import cv from '../assets/CV-Cichocki.pdf'
 
@@ -10,37 +10,50 @@ const Hero = () => {
     <div className="hero">
       <section className="section-title">
         <h1 className="section-title-text">
-          MY PORTFOLIO
+         My Portfolio
         </h1> 
       </section>
       <section className="hero-banner">
         <article className="hero-banner-title">
-          <Tilt className="hero-banner-name">
-              <h2 className="hero-banner-name-one name">AREK</h2>
-              <h3 className="hero-banner-name-two name">CICHOCKI</h3>
-            </Tilt>
-            <div className="hero-banner-logo">
+          <h2 className="hero-banner-hello">Hello, <span>I am</span> Arek</h2>
+            {/* <div className="hero-banner-logo">
               <img src={logo} alt="AC95" className="hero-banner-logo-img" />
+            </div> */}
+            <div className="hero-banner-name">
+              <h2 className="hero-banner-name-one">FRONTEND</h2>
+              <h3 className="hero-banner-name-two">DEVELOPER</h3>
             </div>
-            <Tilt className="hero-banner-name">
-              <h2 className="hero-banner-name-one software">FRONTEND</h2>
-              <h3 className="hero-banner-name-two software">DEVELOPER</h3>
-            </Tilt>
+
+            <div className="hero-banner-buttons">
+              <a href={cv} download="CV-Cichocki" className="button-cv">
+                <span>CV</span>
+              </a>
+              <a href="https://github.com/ArexSpeed" target="_blank" className="button-git">
+                  <span><AiFillGithub/></span>
+              </a>
+            </div>
+
+            <div className="hero-banner-desc">
+             Welcome ! I am glad that you are here on my porfolio website. Here you can find out about me, my story and see all my projects. So let’s go, I hope you enjoy it ! 
+            </div>
+
+              
           </article>
-          <article className="hero-banner-img">
-              <a href="https://github.com/ArexSpeed" target="_blank" className="hero-banner-img-box logo">
-                <span><AiFillGithub/></span>
-              </a>
-              <a href={cv} download="CV-Cichocki" className="hero-banner-img-box cv">
-              <span>CV</span>
-              </a>
-              <div className="hero-banner-img-box pic">
-                  <img src={profile} alt="" />
-              </div>
+          <article className="hero-image">
+            <div className="square">
+              <img src={profile} alt="AC" />
+            </div>
           </article>
       </section>
-      <section className="hero-desc">
-        Welcome ! I am glad that you are here on my porfolio website. Here you can find out about me, my story and see all my projects. So let’s go, I hope you enjoy it ! 
+      <section className="hero-next">
+        <Link 
+          to="about" 
+          smooth={true}
+          duration={500} 
+          spy={true}
+        >
+          <svg className="hero-next-icon" style={{ width: '50px', height: '50px', color: 'white'}} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+        </Link>
       </section>
     </div>
   )
